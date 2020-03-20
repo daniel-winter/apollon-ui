7b725d230e179c6ce8434a35136afd5685d60df4
danielwinter <designwinter@gmail.com>
2020-03-20 13:09:04 +0100

    add first props to button

--- packages/ui-components/Button/Button.js
+++ packages/ui-components/Button/Button.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-const Button = () => {
+const Button = ({
+    className = null,
+}) => {
+
+    const buttonAttributes = {
+        className: classnames(styles.button, className),
+    };
   return (
-    <button>Hello Button</button>
+    <button {...buttonAttributes}>Hello Button</button>
   );
+}
+
+Button.propTypes = {
+    className: PropTypes.string,
+    content: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+    size: PropTypes.oneOf(['regular', 'large']),
+    type: PropTypes.oneOf(['button', 'submit', 'reset']),
 }
 
 export default Button;
